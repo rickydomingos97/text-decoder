02b86052a3525c5ae77303294861fd994b64d18f
Ricardo Domingos <ricardodomingos97@gmail.com>
2024-03-15 09:41:35 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Decodificador de texto
+
+projeto desenvolvido no curso da Oracle One em parceria com a Alura para o desenvolvimento de habilidades em HTML, CSS e JS
---
+++ script.js
@@ -0,0 +1,59 @@
+function criptografarTexto(texto) {
+  return texto.replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/a/g, 'ai')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat');
+}
+
+function descriptografarTexto(texto) {
+  return texto.replace(/ufat/g, 'u')
+    .replace(/ober/g, 'o')
+    .replace(/ai/g, 'a')
+    .replace(/imes/g, 'i')
+    .replace(/enter/g, 'e');
+}
+function letraMaiuscula() {
+  var texto = document.getElementById("text").value;
+  var regex = /[A-Z]/;
+  if (regex.test(texto)) {
+    document.querySelector(".dalek").style.display = "block";
+  } else {
+    document.querySelector(".dalek").style.display = "none";
+  }
+}
+function verificarAcentos(texto) {
+  var regex = /[áàãâéèêíïóôõöúç]/i;
+  return regex.test(texto);
+}
+
+function criptografar() {
+  var texto = document.getElementById("text").value;
+  var regex = /[A-Z]/;
+  if (!regex.test(texto) && !verificarAcentos(texto)) {
+    var textoCriptografado = criptografarTexto(texto);
+    document.getElementById("resposta").innerHTML = textoCriptografado;
+    document.querySelector(".dalek").style.display = "none";
+  } else {
+    document.querySelector(".dalek").style.display = "block";
+  }
+  document.getElementById("text").value = "";
+}
+
+function descriptografar() {
+  var texto = document.getElementById("text").value;
+  var textoDescriptografado = descriptografarTexto(texto);
+  document.getElementById("resposta").innerHTML = textoDescriptografado;
+  document.getElementById("text").value = "";
+
+}
+function copiar() {
+  var textoResposta = document.getElementById("resposta").innerText;
+  navigator.clipboard.writeText(textoResposta)
+    .then(() => {
+      alert("Texto copiado com sucesso!");
+    })
+}
+
+
+
